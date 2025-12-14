024f2f9d3555d9a75acb21be0ea861b98acc0ffa
Junior Ramos <ramosjr729@gmail.com>
2025-12-14 14:56:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import { entrada } from "readline-sync";
-let nome = entrada.question('informe seu nome: ');
-console.log(nome);
+import { entradaDados } from "readline-sync";
+
+console.log('aplicação de juros');
+
+let valor_divida = entradaDados.question('informe o valor da divida: R$ ');
+let dias_vencimento = entradaDados.question('informe os dias de atraso: ');
+
+console.log("\nValor original da dívida: R$ "+valor_divida);
+console.log("Dias atrasados: "+dias_vencimento);
+console.log("Taxa de Juros: 0%");
+console.log("Valor total com juros: R$ 0");
